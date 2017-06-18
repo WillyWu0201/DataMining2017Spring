7ec48e3f07ad1c2ed9559b81815a65a7e6c4c7ce
LAPTOP-CPD2IF36\user <nchu.si.j@gmail.com>
2017-06-18 14:27:30 +0800

    合併成一個CSV檔

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from pandas import read_csv
 from pandas import datetime
 from pandas import DataFrame
 from pandas import concat
-
+import pandas
 
 # frame a sequence as a supervised learning problem
 def timeseries_to_supervised(data, lag=1):
@@ -19,20 +19,22 @@ def parser(x):
     return datetime.strptime('190' + x, '%Y-%m')
 
 # series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-series = read_csv('201001.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+#series = read_csv('201001.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
 # transform to supervised learning
-X = series.values
-supervised = timeseries_to_supervised(X, 1)
+#X = series.values
+#supervised = timeseries_to_supervised(X, 1)
 # print(supervised.head())
+
 
 #取出2010~2016大里的資訊
 for year in range(2010, 2016):
     for month in range(1, 12):
-        filePath = 'AQX_reName/%d%02d.csv' % (year, month)
+        filePath = 'D:\AQX_reName/%d%02d.csv' % (year, month)
         # print(filePath)
         df = read_csv(filePath, header=None)
-        dfzzz = df[(df[1] == 'Taichung') & (df[3] == 'Big mile')]
-        print(dfzzz)
+        dfTC = df[(df[1] == 'Taichung') & (df[3] == 'Big mile')]
+        print(dfTC)
+        dfTC.to_csv('testcsv.csv', mode= 'a',header=False,index=False)
 #
 # df = read_csv('AQX_reName/201001.csv', header = None)
 # dfzzz = df[(df[1] == 'Taichung') & (df[3] == 'Big mile')]
